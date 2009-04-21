80f313bf41c892e1349f118e61f3d1234efaca44
Loiseleur Michel <mloiseleur@linagora.com>
2009-04-21 11:42:48 +0200

    far better to use creation date

--- db/migrate/20090421092656_restore_issue_updated_on.rb
+++ db/migrate/20090421092656_restore_issue_updated_on.rb
@@ -4,7 +4,7 @@ class RestoreIssueUpdatedOn < ActiveRecord::Migration
     Issue.all.each do |i|
       comment = i.last_comment
       comment = i.comments.last if comment.nil?
-      i.update_attribute(:updated_on, comment.updated_on)
+      i.update_attribute(:updated_on, comment.created_on)
     end
     Issue.record_timestamps = true
   end
