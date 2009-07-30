a3c29dd07eb5fcab268b2fcd4e883d682c3339ad
Loiseleur Michel <mloiseleur@linagora.com>
2009-07-30 16:20:37 +0200

    fix initial settings

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,6 +1,8 @@
-ActionMailer::Base.smtp_settings = {
-  :address  => Setting.email_server,
-  :port  => Setting.email_port,
-  :domain  => Setting.email_domain
-}
-ActionMailer::Base.default_charset = Setting.email_charset
+if Setting.table_exists? # allows to create database
+  ActionMailer::Base.smtp_settings = {
+    :address  => Setting.email_server,
+    :port  => Setting.email_port,
+    :domain  => Setting.email_domain
+  }
+  ActionMailer::Base.default_charset = Setting.email_charset
+end
