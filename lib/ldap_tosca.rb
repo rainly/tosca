1ffbefd8a986979cd2829e0615aaea5f5f2be2ca
Loiseleur Michel <mloiseleur@linagora.com>
2009-08-14 16:48:20 +0200

    there's really a LOT of cases in an LDAP authentication process

--- lib/ldap_tosca.rb
+++ lib/ldap_tosca.rb
@@ -22,21 +22,6 @@ module LdapTosca
     end
 
     READ_ATTRIBUTES = %w(uid cn mail mobile modifyTimestamp)
-
-    # Authentificate the user
-    # Can throw an Exception if LDAP is unreachable, via connect method
-    def authentificate_user(login, password)
-      user = nil
-      ldap_conn = self.connect(login, password) do |conn|
-        res = conn.search2(Setting.ldap_basedn,
-                           LDAP::LDAP_SCOPE_ONELEVEL,
-                           Setting.ldap_filter % login,
-                           READ_ATTRIBUTES)
-        user = res.first unless res.empty?
-      end
-      user
-    end
-
     TS_FORMAT = 'a4a2a2a2a2a2'
 
     # Key method, coming from User AR model
@@ -45,23 +30,20 @@ module LdapTosca
       # customer authentication is not on ldap
       return authenticate_without_ldap(login, pass) if user and user.recipient?
 
-      ldap_user = nil
-      begin
-        ldap_user = self.authentificate_user(login, pass)
-      rescue
-        # fallback when ldap is down or not reachable
-        return authenticate_without_ldap(login, pass)
-      end
+      ldap_conn = self.connect2ldap
+      return authenticate_without_ldap(login, pass) unless ldap_conn
 
-      # expert access can be disabled directly in ldap
+      ldap_user = self.search_user(ldap_conn, login)
       return nil unless check_access(user, ldap_user)
-      # fallback when ldap search failed (invalid dn, for instance)
       return authenticate_without_ldap(login, pass) unless ldap_user
+
+      ldap_auth = self.authenticate_user(ldap_conn, login, pass)
+      return authenticate_without_ldap(login, pass) unless ldap_auth
 
       ts = Time.utc(*ldap_user['modifyTimestamp'].
                 first.unpack(TS_FORMAT)).localtime
       if user.nil? or user.updated_on.nil? or ts > user.updated_on
-        update_account_from_ldap(user, ldap_user, pass)
+        self.update_account_from_ldap(user, ldap_user, pass)
       end
       user
     end
@@ -96,24 +78,39 @@ module LdapTosca
     #Connect to the LDAP and handle errors
     #Takes a bloc which yields the connection
     #Return nil if there was a problem
-    def connect(login, pwd)
+    def connect2ldap(login, pwd)
       ldap_conn = LDAP::Conn.new(Setting.ldap_host, Setting.ldap_port.to_i)
       ldap_conn.set_option(LDAP::LDAP_OPT_PROTOCOL_VERSION, Setting.ldap_protocol.to_i)
       begin
         ldap_conn.start_tls.nil?
       rescue LDAP::Error => e
         logger.error "LDAP connection problem : #{e}"
-        raise "Connection problem"
-      end
-      begin
-        ldap_conn.bind(Setting.ldap_binddn % login, pwd)
-        yield(ldap_conn) if ldap_conn.bound?
-        ldap_conn.unbind
-      rescue LDAP::ResultError
-        ldap_conn.unbind if ldap_conn.bound?
         ldap_conn = nil
       end
       ldap_conn
     end
+
+    # lookup an user, without credentials
+    def search_user(ldap_conn, login)
+      res = ldap_conn.search2(Setting.ldap_basedn,
+                              LDAP::LDAP_SCOPE_ONELEVEL,
+                              Setting.ldap_filter % login,
+                              READ_ATTRIBUTES)
+      (res.empty? ? nil : res.first)
+    end
+
+    # try credential
+    def authenticate_user(ldap_conn, login, pwd)
+      authentication_succesful = false
+      begin
+        ldap_conn.bind(Setting.ldap_binddn % login, pwd)
+        authentication_succesful = true
+        ldap_conn.unbind
+      rescue LDAP::ResultError
+        ldap_conn.unbind if ldap_conn.bound?
+      end
+      authentication_succesful
+    end
+
   end
 end
