614e0a4103adcb6dedead155094dbef75f789bcc
Patrick Guiran <pguiran@linagora.com>
2009-08-31 14:43:28 +0200

    + html2text : add _ for underlined text  + html2text : link replace don't repeate the same link at the end of the mail

--- lib/utils.rb
+++ lib/utils.rb
@@ -66,8 +66,13 @@ def html2text(html)
   links = []
   linkregex = /<[^>]*(src|href)=\s*(.?)([^>\s]*)\2[^>]*>([^>]*)<[^>]*>/i
   while linkregex.match(text)
-    links << $~[3]
-    text.sub!(linkregex, "#{$~[4]}[#{links.size}]")
+    index = links.index($~[3])
+    # have we already see this link ? (don't repeat the same link in the stack)
+    if index.nil?
+      links << $~[3]
+      index = links.size
+    end
+    text.sub!(linkregex, "#{$~[4]}[#{index}]")
   end
 
   text = CGI.unescapeHTML(
@@ -83,7 +88,7 @@ def html2text(html)
       gsub(/<\/pre(| [^>]*)>/i, "\n").
       gsub(/<\/?(b|strong)[^>]*>/i, "*").
       gsub(/<\/?(i|em)[^>]*>/i, "/").
-      gsub(/<\/?u[^>]*>/i, "").
+      gsub(/<\/?u[^>]*>/i, "_").
       gsub(/<[^>]*>/, '')
   )
   # OOo does not know the Unbreakable UTF-8 char, as of OOo 2.4.1, Hardy.
