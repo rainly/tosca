7d4ab7066df15865c69339b53854739eb1d34729
Loiseleur Michel <mloiseleur@linagora.com>
2009-10-13 17:16:39 +0200

    fix broken migration

--- db/migrate/20090825175316_add_taken_into_account_delay_parameter_in_contract.rb
+++ db/migrate/20090825175316_add_taken_into_account_delay_parameter_in_contract.rb
@@ -1,5 +1,8 @@
 class AddTakenIntoAccountDelayParameterInContract < ActiveRecord::Migration
 
+  class Contract < ActiveRecord::Base
+  end
+
   def self.up
     add_column :contracts, :taken_into_account_delay, :integer, :default => 0, :null => false
     Contract.all.each { |c| c.update_attribute :taken_into_account_delay, 1 }
